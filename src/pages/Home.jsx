828a22c1b9359fc1ef34c5decd75eb0b98d8d93d
ajuteixeira <teixeirajuuh@gmail.com>
2024-02-06 18:17:52 -0300

    ajustes no design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-      <section className="h-full flex flex-col items-center justify-start gap-12 bg-center bg-cover bg-no-repeat bg-fixed bg-[url('./assets/images/bg.jpg')]">
+      <section className="h-full flex flex-col items-center justify-start gap-8 bg-center bg-cover bg-no-repeat bg-fixed bg-[url('./assets/images/bg.jpg')]">
         <Header />
         <VisitorForm />
         <Footer />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 import LoginForm from "../components/LoginForm";
 
 export default function Login() {
@@ -8,7 +7,6 @@ export default function Login() {
       <section className="h-screen flex flex-col items-center justify-start gap-14 bg-center bg-cover bg-no-repeat bg-fixed bg-[url('./assets/images/bg.jpg')]">
         <Header />
         <LoginForm />
-        <Footer />
       </section>
     </>
   );
